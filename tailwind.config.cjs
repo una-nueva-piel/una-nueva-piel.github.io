cd5b89c3bc5356f877aadf4305a508276c18896d
Lucas Leonardo <lucasleonardo@protonmail.com>
2023-07-14 18:43:37 -0300

    misc: fix bug, add font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,9 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		fontFamily: {
+			'serif': ['Piazzolla', 'ui-sans-serif', 'system-ui'],
+		},
 		extend: {},
 	},
 	plugins: [],
