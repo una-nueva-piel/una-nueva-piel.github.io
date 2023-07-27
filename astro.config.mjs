02ce041ba955d9253e1e157a605e8ed4790c2423
Lucas Leonardo <lucasleonardo@protonmail.com>
2023-07-27 16:46:23 -0300

    ci/cd: add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs()]
+  site: "https://carpiano.github.io",
+  base: "/mano-bionica-2",
+  integrations: [tailwind(), alpinejs()],
 });
