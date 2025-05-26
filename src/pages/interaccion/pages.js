da8b86d51baf506fd65eedac19a5308e96704c4b
Miguel Pagano <miguel.pagano@gmail.com>
2025-05-26 01:16:12 -0300

    Correcciones de modularización

---
+++ src/pages/interaccion/pages.js
@@ -0,0 +1,28 @@
+const pages = [
+    { id: "", title: "", color: "", video: "" },
+    {
+        id: "ver_y_ser_visto",
+        title: "Ver y ser visto",
+        color: "green",
+        video: "https://a8cd5793-e749-468c-8aff-a682d1706ec0.mdnplay.dev/shared-assets/videos/flower.webm",
+    },
+    {
+        id: "respirar",
+        title: "Respirar",
+        color: "red",
+        video: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    },
+    {
+        id: "nuevas_pieles",
+        title: "Nuevas pieles",
+        color: "blue",
+        video: "/fragmento12.webm",
+    },
+    {
+        id: "que_hacemos",
+        title: "Qué hacemos",
+        color: "yellow",
+        video: "/fragmento12.webm",
+    },
+];
+export default pages;
