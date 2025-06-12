de92bc5dbf678390d89c58b7d623aa0aef400ac1
Miguel Pagano <miguel.pagano@gmail.com>
2025-06-12 17:59:33 -0300

    fondo

--- src/pages/interaccion/pages.js
+++ src/pages/interaccion/pages.js
@@ -4,25 +4,25 @@ const pages = [
         id: "ver_y_ser_visto",
         title: "Ver y ser visto",
         color: "green",
-        video: "/fondo/animacion.gif",
+        video: "/animacion.gif",
     },
     {
         id: "respirar",
         title: "Respirar",
         color: "red",
-        video: "/fondo/animacion.gif",
+        video: "/animacion.gif",
     },
     {
         id: "nuevas_pieles",
         title: "Nuevas pieles",
         color: "blue",
-        video: "/fondo/animacion.gif",
+        video: "/animacion.gif",
     },
     {
         id: "que_hacemos",
         title: "Qué hacemos",
         color: "yellow",
-        video: "/fondo/animacion.gif",
+        video: "/animacion.gif",
     },
 ];
 export default pages;
