6934fc426c8bde992ed7f8eba0f7454f7f450e57
Miguel Pagano <miguel.pagano@gmail.com>
2025-06-12 16:23:25 -0300

    fondo con gif animado.

--- src/pages/interaccion/pages.js
+++ src/pages/interaccion/pages.js
@@ -4,25 +4,25 @@ const pages = [
         id: "ver_y_ser_visto",
         title: "Ver y ser visto",
         color: "green",
-        video: "https://a8cd5793-e749-468c-8aff-a682d1706ec0.mdnplay.dev/shared-assets/videos/flower.webm",
+        video: "/fondo/animacion.gif",
     },
     {
         id: "respirar",
         title: "Respirar",
         color: "red",
-        video: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        video: "/fondo/animacion.gif",
     },
     {
         id: "nuevas_pieles",
         title: "Nuevas pieles",
         color: "blue",
-        video: "/fragmento12.webm",
+        video: "/fondo/animacion.gif",
     },
     {
         id: "que_hacemos",
         title: "Qué hacemos",
         color: "yellow",
-        video: "/fragmento12.webm",
+        video: "/fondo/animacion.gif",
     },
 ];
 export default pages;
